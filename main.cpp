f362cf72ad8025b1f000cc3579a9c425b66e7b01
大嶋大雅 <k023g0122@g.neec.ac.jp>
2025-04-14 15:28:46 +0900

    エラーの消去

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+#pragma warning(push)
+#pragma warning(disable:28251)
 #include <Windows.h>
+#pragma warning(pop)
 
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
