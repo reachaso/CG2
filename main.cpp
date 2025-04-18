797864b1de6983b8c15c88a008925b59e9413f95
大嶋大雅 <k023g0122@g.neec.ac.jp>
2025-04-18 15:55:15 +0900

    Revert "Crashした時にファイル生成をするように"
    This reverts commit 33b7097d3e4ab93dd6b6d61ebcd25e78691fddb6.

--- main.cpp
+++ main.cpp
@@ -10,9 +10,6 @@
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include <cassert>
-#include <DbgHelp.h>
-#include <strsafe.h>
-#pragma comment(lib, "Dbghelp.lib") //DbgHelp.libをリンクする
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma warning(pop)
@@ -78,39 +75,9 @@ std::string ConvertString(const std::wstring& wstr) {
 	return str;
 }
 
-static LONG WINAPI ExportDump(PEXCEPTION_POINTERS exception) {
-  // 時刻を取得して、時刻を名前に入れたファイルを作成。Dumpsのディレクトリ以下に出力
-  SYSTEMTIME time;
-  GetLocalTime(&time);
-  wchar_t filePath[MAX_PATH] = {0};
-  CreateDirectory(L"./Dumps", nullptr);
-  StringCchPrintfW(filePath, MAX_PATH, L"./Dumps/%04d-%02d%02d-%02d%02d.dmp",
-                   time.wYear, time.wMonth, time.wDay, time.wHour,
-                   time.wMinute);
-  HANDLE dumpFileHandle =
-      CreateFile(filePath, GENERIC_READ | GENERIC_WRITE,
-                 FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
-  // processId(このexeのId)とクラッシュ(例外)の発生したthreadIdを取得
-  DWORD processId = GetCurrentProcessId();
-  DWORD threadId = GetCurrentThreadId();
-  // 設定情報を入力
-  MINIDUMP_EXCEPTION_INFORMATION minidumpInformation{0};
-  minidumpInformation.ThreadId = threadId;
-  minidumpInformation.ExceptionPointers = exception;
-  minidumpInformation.ClientPointers = TRUE;
-  // Fumpを出力。MiniDumpNormalは最低限の情報を出力するフラグ
-  MiniDumpWriteDump(GetCurrentProcess(), processId, dumpFileHandle,
-                    MiniDumpNormal, &minidumpInformation, nullptr, nullptr);
-  // 他に関連づけられているSSH例外ハンドラがあれば実行。通常はプロセスを終了する
-	return EXCEPTION_EXECUTE_HANDLER;
-}
-
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	//誰も補足しない例外をキャッチするための関数を登録
-	SetUnhandledExceptionFilter(ExportDump);
-
 	//ログのディレクトリを用意
 	std::filesystem::create_directory("logs");
 	//現在の時間を取得(UTC)
