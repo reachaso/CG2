1c3a4b3f085953aee7fd0ed1db188a923950642e
大嶋大雅 <k023g0122@g.neec.ac.jp>
2025-10-06 22:08:12 +0900

    spriteクラスの実装とmySoourcesファイルの名前を変更

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #pragma warning(push)
 #pragma warning(disable : 28251)
 #include "DebugCamera/DebugCamera.h"
-#include "DirectXTex/DirectXTex.h"
 #include "Dx12Core.h"
 #include "GraphicsPipeline/GraphicsPipeline.h"
 #include "Input/Input.h"
@@ -11,13 +10,12 @@
 #include "Model3D/Model3D.h"
 #include "Sound/Sound.h"
 #include "Sphere/Sphere.h"
+#include "Sprite2D/Sprite2D.h"
 #include "Texture/Texture2D/Texture2D.h"
 #include "Texture/TextureManager/TextureManager.h"
 #include "Window/Window.h"
 #include "function/function.h"
 #include "imgui/imgui.h"
-#include "imgui/imgui_impl_dx12.h"
-#include "imgui/imgui_impl_win32.h"
 #include "ImGuiManager/ImGuiManager.h"
 #include "struct.h"
 #include <Windows.h>
@@ -25,9 +23,6 @@
 #include <cstdint>
 #include <d3d12.h>
 #include <dxcapi.h>
-#include <dxgi1_6.h>
-#include <filesystem> //ファイルやディレクトリの操作を行うライブラリ
-#include <format>     //文字列のフォーマットを行うライブラリ
 
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
@@ -130,9 +125,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     directionalLight.direction.z /= len;
   }
 
-  Sound Alarm01 = Sound();
-  Alarm01.Initialize("Resources/Sounds/Alarm01.wav"); // サウンドの初期化
-
   DebugCamera debugCamera;
   debugCamera.Initialize(&input,
                          0.45f, // FOV
@@ -150,6 +142,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
                         float(kClientWidth) / kClientHeight, 0.1f,
                         100.0f // nearZ, farZ
   );
+
+  //===============================
+  // Textureをよんで転送する
+  //===============================
+  TextureManager texMgr;
+  texMgr.Init(device, &core.SRV());
+
+  // sRGBに寄せたいので true でロード（法線等のデータテクスチャは false 推奨）
+  int tex_uvChecker = texMgr.LoadID("Resources/uvChecker.png", true);
+  int tex_monsterBall = texMgr.LoadID("Resources/monsterBall.png", true);
+  int tex_checker = texMgr.LoadID("Resources/checkerBoard.png", true);
+  int tex_white = texMgr.LoadID("Resources/white1x1.png", true);
 
   //===========================
   // Sphere用
@@ -198,111 +202,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   directionalLightResource->Map(
       0, nullptr, reinterpret_cast<void **>(&directionalLightData));
   *directionalLightData = directionalLight; // 初期値をコピー
-
-  //===========================
-  // sprite用の頂点リソースを作る
-  //===========================
-
-  ID3D12Resource *vertexResourceSprite =
-      CreateBufferResource(device, sizeof(VertexData) * 4);
-
-  // 頂点バッファビューを作成する
-  D3D12_VERTEX_BUFFER_VIEW vertexBufferViewSprite{};
-  // リソースの先頭のアドレスから使う
-  vertexBufferViewSprite.BufferLocation =
-      vertexResourceSprite->GetGPUVirtualAddress();
-  // 使用するリソースのサイズは頂点4つ分のサイズ
-  vertexBufferViewSprite.SizeInBytes = sizeof(VertexData) * 4;
-  // 1つの頂点のサイズ
-  vertexBufferViewSprite.StrideInBytes = sizeof(VertexData);
-
-  // 頂点データを設定する
-  VertexData *vertexDataSprite = nullptr;
-  vertexResourceSprite->Map(0, nullptr,
-                            reinterpret_cast<void **>(&vertexDataSprite));
-
-  // 4点の情報
-  vertexDataSprite[0].position = Vector4(0.0f, 360.0f, 0.0f, 1.0f); // 左下
-  vertexDataSprite[0].texcoord = Vector2(0.0f, 1.0f); // 左下のテクスチャ座標
-  vertexDataSprite[0].normal = Vector3(0.0f, 0.0f, -1.0f);
-  vertexDataSprite[1].position = Vector4(0.0f, 0.0f, 0.0f, 1.0f); // 左上
-  vertexDataSprite[1].texcoord = Vector2(0.0f, 0.0f); // 左上のテクスチャ座標
-  vertexDataSprite[1].normal = Vector3(0.0f, 0.0f, -1.0f);
-  vertexDataSprite[2].position = Vector4(640.0f, 360.0f, 0.0f, 1.0f); // 右下
-  vertexDataSprite[2].texcoord = Vector2(1.0f, 1.0f); // 右下のテクスチャ座標
-  vertexDataSprite[2].normal = Vector3(0.0f, 0.0f, -1.0f);
-  vertexDataSprite[3].position = Vector4(640.0f, 0.0f, 0.0f, 1.0f); // 右上
-  vertexDataSprite[3].texcoord = Vector2(1.0f, 0.0f); // 右上のテクスチャ座標
-  vertexDataSprite[3].normal = Vector3(0.0f, 0.0f, -1.0f);
-
-  // Transform
-  ID3D12Resource *transformationMatrixResourceSprite =
-      CreateBufferResource(device, sizeof(TransformationMatrix));
-  // データを書き込む
-  TransformationMatrix *transformationMatrixDataSprite = nullptr;
-  // 書き込むためのアドレスを取得
-  transformationMatrixResourceSprite->Map(
-      0, nullptr, reinterpret_cast<void **>(&transformationMatrixDataSprite));
-  // 単位行列を設定
-  transformationMatrixDataSprite->WVP = MakeIdentity4x4();
-  transformationMatrixDataSprite->World = MakeIdentity4x4();
-
-  Transform transformSprite{
-      {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}};
-
-  // Sprite用のWorldMatrixを作る
-  Matrix4x4 worldMatrixSprite =
-      MakeAffineMatrix(transformSprite.scale, transformSprite.rotation,
-                       transformSprite.translation);
-  Matrix4x4 viewMatrixSprite = MakeIdentity4x4();
-  Matrix4x4 projectionMatrixSprite = MakeOrthographicMatrix(
-      0.0f, 0.0f, float(kClientWidth), float(kClientHeight), 0.0f, 100.0f);
-  Matrix4x4 WVPMatrixSprite = Multiply(
-      worldMatrixSprite, Multiply(viewMatrixSprite, projectionMatrixSprite));
-  transformationMatrixDataSprite->WVP = WVPMatrixSprite; // データを設定
-
-  // sprite用のMaterialを作成する
-  ID3D12Resource *spriteMaterialResource =
-      CreateBufferResource(device, sizeof(Material));
-  Material *spriteMaterialData = nullptr;
-  spriteMaterialResource->Map(0, nullptr,
-                              reinterpret_cast<void **>(&spriteMaterialData));
-  // Materialの初期化
-  spriteMaterialData->color = Vector4(1.0f, 1.0f, 1.0f, 1.0f); // 白色
-  spriteMaterialData->uvTransform = MakeIdentity4x4();
-
-  // インデックス用バッファリソースを作成（6頂点分の uint32_t インデックス）
-  ID3D12Resource *indexResourceSprite =
-      CreateBufferResource(device, sizeof(uint32_t) * 6);
-
-  // インデックスバッファビューを作成
-  D3D12_INDEX_BUFFER_VIEW indexBufferViewSprite{};
-  indexBufferViewSprite.BufferLocation =
-      indexResourceSprite->GetGPUVirtualAddress(); // バッファの先頭アドレス
-  indexBufferViewSprite.SizeInBytes =
-      sizeof(uint32_t) * 6; // インデックス数×サイズ
-  indexBufferViewSprite.Format = DXGI_FORMAT_R32_UINT;
-
-  // インデックスリソースにデータを転送する
-  uint32_t *indexDataSprite = nullptr;
-  indexResourceSprite->Map(0, nullptr,
-                           reinterpret_cast<void **>(&indexDataSprite));
-
-  indexDataSprite[0] = 0; // 左下
-  indexDataSprite[1] = 1; // 左上
-  indexDataSprite[2] = 2; // 右下
-  indexDataSprite[3] = 1; // 左上
-  indexDataSprite[4] = 3; // 右上
-  indexDataSprite[5] = 2; // 右上
-
-  //===========================
-  // uv用
-  //===========================
-
-  Transform uvTransformSprite{
-      {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}};
-
-  bool isSprite = false;
 
   //==========================
   // VertexResourceを生成する
@@ -447,55 +346,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   bool isTriangle2 = false;
 
   //===============================
-  // ModelData
-  //===============================
-
-  bool isModelPlane = false;
-  bool enableLighting = true;
-
-  int modelIndex = 0;
-
-  // ================
-  // Model3D 準備
-  // ================
-  Model3D modelPlane, modelAxis, modelTeapot, modelBunny, modelSuzanne;
-
-  modelPlane.Initialize(device);
-  modelPlane.LoadObjGeometryLikeFunction("Resources", "plane.obj");
-
-  modelAxis.Initialize(device);
-  modelAxis.LoadObjGeometryLikeFunction("Resources", "axis.obj");
-
-  modelTeapot.Initialize(device);
-  modelTeapot.LoadObjGeometryLikeFunction("Resources", "teapot.obj");
-  modelTeapot.SetLightingMode(None);
-
-  modelBunny.Initialize(device);
-  modelBunny.LoadObjGeometryLikeFunction("Resources", "bunny.obj");
-
-  modelSuzanne.Initialize(device);
-  modelSuzanne.LoadObjGeometryLikeFunction("Resources", "suzanne.obj");
-  // suzanne の球面UV付与（旧mainの処理をクラスに移したメソッド）
-  modelSuzanne.EnsureSphericalUVIfMissing();
-
-  //===============================
   // Imguiの初期化
   //===============================
 
   ImGuiManager imgui;
   imgui.Init(window.hwnd, core);
 
-  //===============================
-  // Textureをよんで転送する
-  //===============================
-  TextureManager texMgr;
-  texMgr.Init(device, &core.SRV());
-
-  // sRGBに寄せたいので true でロード（法線等のデータテクスチャは false 推奨）
-  int tex_uvChecker = texMgr.LoadID("Resources/uvChecker.png", true);
-  int tex_monsterBall = texMgr.LoadID("Resources/monsterBall.png", true);
-  int tex_checker = texMgr.LoadID("Resources/checkerBoard.png", true);
-  int tex_white = texMgr.LoadID("Resources/white1x1.png", true);
+  
 
   // 0: uvChecker, 1: モンスターボール , 2: checkerBoard, 3: white1x1
   int triangleTextureIndex = 0;
@@ -661,104 +518,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
           ImGui::EndTabItem();
         }
 
-        if (ImGui::BeginTabItem("スプライト")) {
-
-          ImGui::Dummy(ImVec2(0.0f, 5.0f));
-
-          ImGui::Checkbox("スプライトの表示", &isSprite);
-
-          ImGui::Dummy(ImVec2(0.0f, 5.0f));
-
-          if (isSprite) {
-
-            // スプライトの描画設定
-            if (ImGui::BeginTabBar("設定")) {
-              if (ImGui::BeginTabItem("スプライトの設定")) {
-
-                if (ImGui::CollapsingHeader("位置",
-                                            ImGuiTreeNodeFlags_DefaultOpen)) {
-                  ImGui::SliderFloat2("##Translation",
-                                      &transformSprite.translation.x, 0.0f,
-                                      1080.0f);
-                  if (ImGui::Button("位置をリセット")) {
-                    transformSprite.translation = {0.0f, 0.0f, 0.0f};
-                  }
-                }
-
-                ImGui::Dummy(ImVec2(0.0f, 5.0f));
-
-                if (ImGui::CollapsingHeader("回転",
-                                            ImGuiTreeNodeFlags_DefaultOpen)) {
-                  ImGui::SliderFloat3("##Rotation", &transformSprite.rotation.x,
-                                      0.0f, 10.0f);
-                  if (ImGui::Button("回転をリセット")) {
-                    transformSprite.rotation = {0.0f, 0.0f, 0.0f};
-                  }
-                }
-
-                ImGui::Dummy(ImVec2(0.0f, 5.0f));
-
-                if (ImGui::CollapsingHeader("大きさ",
-                                            ImGuiTreeNodeFlags_DefaultOpen)) {
-                  ImGui::SliderFloat2("##Scale", &transformSprite.scale.x, 0.0f,
-                                      10.0f);
-
-                  if (ImGui::Button("大きさをリセット")) {
-                    transformSprite.scale = {1.0f, 1.0f, 1.0f};
-                  }
-                }
-                ImGui::EndTabItem();
-              }
-
-              if (ImGui::BeginTabItem("テクスチャの設定")) {
-
-                if (ImGui::CollapsingHeader("テクスチャの表示位置",
-                                            ImGuiTreeNodeFlags_DefaultOpen)) {
-
-                  ImGui::DragFloat2("##テクスチャの表示位置",
-                                    &uvTransformSprite.translation.x, 0.01f,
-                                    -10.0f, 10.0f);
-
-                  if (ImGui::Button("テクスチャの位置をリセット")) {
-                    uvTransformSprite.translation = Vector3(0.0f, 0.0f, 0.0f);
-                  }
-
-                  ImGui::Dummy(ImVec2(0.0f, 5.0f));
-                }
-
-                if (ImGui::CollapsingHeader("テクスチャの大きさ",
-                                            ImGuiTreeNodeFlags_DefaultOpen)) {
-                  ImGui::DragFloat2("##テクスチャの大きさ",
-                                    &uvTransformSprite.scale.x, 0.01f, -10.0f,
-                                    10.0f);
-
-                  if (ImGui::Button("テクスチャの大きさをリセット")) {
-                    uvTransformSprite.scale = Vector3(1.0f, 1.0f, 1.0f);
-                  }
-
-                  ImGui::Dummy(ImVec2(0.0f, 5.0f));
-                }
-
-                if (ImGui::CollapsingHeader("テクスチャの回転",
-                                            ImGuiTreeNodeFlags_DefaultOpen)) {
-                  ImGui::SliderAngle("##テクスチャの回転",
-                                     &uvTransformSprite.rotation.z);
-
-                  if (ImGui::Button("テクスチャの回転をリセット")) {
-                    uvTransformSprite.rotation = Vector3(0.0f, 0.0f, 0.0f);
-                  }
-
-                  ImGui::Dummy(ImVec2(0.0f, 5.0f));
-                }
-
-                ImGui::EndTabItem();
-              }
-              ImGui::EndTabBar();
-            }
-          }
-          ImGui::EndTabItem();
-        }
-
         if (ImGui::BeginTabItem("球")) {
 
           ImGui::Dummy(ImVec2(0.0f, 5.0f));
@@ -870,55 +629,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
             }
           }
           ImGui::EndTabItem();
-        }
-
-        if (ImGui::BeginTabItem("3Dモデル")) {
-
-          ImGui::Dummy(ImVec2(0.0f, 5.0f));
-
-          ImGui::Checkbox("3Dモデルの表示", &isModelPlane);
-
-          ImGui::Dummy(ImVec2(0.0f, 5.0f));
-
-          if (isModelPlane) {
-
-            const char *modelItems[] = {"Plane", "axis", "ティーポット",
-                                        "バニー", "スザンヌ"};
-            ImGui::Combo("モデル選択", &modelIndex, modelItems,
-                         IM_ARRAYSIZE(modelItems));
-
-            ImGui::Dummy(ImVec2(0.0f, 5.0f));
-
-            const char *modelTextureItems[] = {
-                "white1x1", "uvChecker", "モンスターボール", "checkerBoard"};
-            ImGui::Combo("テクスチャ選択", &modelTextureIndex,
-                         modelTextureItems, IM_ARRAYSIZE(modelTextureItems));
-
-            ImGui::Dummy(ImVec2(0.0f, 5.0f));
-
-            if (ImGui::BeginTabBar("設定")) {
-
-              if (ImGui::BeginTabItem("3Dモデル設定")) {
-                ImGui::EndTabItem(); // ライト設定タブを終了
-              }
-              ImGui::EndTabBar(); // 3Dモデル - 設定タブを終了
-            }
-          }
-          ImGui::EndTabItem(); // 3Dモデルタブを終了
-        }
-
-        if (ImGui::BeginTabItem("サウンド")) { // サウンドタブを開始
-
-          Alarm01.SoundImGui("Alarm01");
-
-          ImGui::EndTabItem(); // サウンドタブを終了
-        }
-
-        if (ImGui::BeginTabItem("コントローラー")) {
-
-          input.ControllerImGui();
-
-          ImGui::EndTabItem(); // コントローラータブを終了
         }
 
         ImGui::EndTabBar(); // 各種設定タブを終了
@@ -1091,54 +801,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         sphere->Draw(commandList);
       }
 
-      // 置き換え後
-      if (isModelPlane) {
-        // 1) 今選んでるモデルを取る
-        Model3D *selected = (modelIndex == 0)   ? &modelPlane
-                            : (modelIndex == 1) ? &modelAxis
-                            : (modelIndex == 2) ? &modelTeapot
-                            : (modelIndex == 3) ? &modelBunny
-                                                : &modelSuzanne;
-
-        // 3) 使うテクスチャ（SRV）を main 側で選んで渡す
-        D3D12_GPU_DESCRIPTOR_HANDLE srv =
-            (modelTextureIndex == 0) ? texMgr.GetSrv(tex_white) : // white1x1
-                (modelTextureIndex == 1) ? texMgr.GetSrv(tex_uvChecker)
-                                         : // uvChecker
-                (modelTextureIndex == 2) ? texMgr.GetSrv(tex_monsterBall)
-                                         :  // モンボ
-                texMgr.GetSrv(tex_checker); // checkerBoard
-
-        selected->SetTexture(srv);
-
-        // 4) 行列更新 → 描画
-        //    RootParam(0:Material,1:WVP,2:SRV,3:Light) のセットや VB セットは
-        //    Draw() が内部で行う
-        selected->Update(view, proj);
-        selected->Draw(commandList);
-      }
-
-      if (isSprite) {
-
-        // spriteのMaterial用CBVを設定
-        commandList->SetGraphicsRootConstantBufferView(
-            0, spriteMaterialResource->GetGPUVirtualAddress());
-        // spriteのWVP用CBVを設定
-        commandList->SetGraphicsRootConstantBufferView(
-            1, transformationMatrixResourceSprite->GetGPUVirtualAddress());
-        // spriteのSRVを設定
-        commandList->SetGraphicsRootDescriptorTable(
-            2, texMgr.GetSrv(tex_uvChecker)); // テクスチャのSRVを設定
-
-        // --- スプライト描画 ---
-        commandList->IASetVertexBuffers(0, 1, &vertexBufferViewSprite);
-        commandList->IASetIndexBuffer(&indexBufferViewSprite);
-        commandList->SetGraphicsRootConstantBufferView(
-            1, transformationMatrixResourceSprite->GetGPUVirtualAddress());
-        commandList->DrawIndexedInstanced(6, 1, 0, 0,
-                                          0); // 6頂点、開始インデックス0
-      }
-
       imgui.Render(commandList);
 
       core.EndFrame();
@@ -1166,27 +828,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
       wvpData->WVP = wvpMatrix;
       wvpData->World = worldMatrix;
-
-      //======================================
-      // スプライト用の行列・UV変換の更新
-      //======================================
-
-      // --- Sprite用のWVP行列を更新 ---
-      Matrix4x4 worldMatrixSprite =
-          MakeAffineMatrix(transformSprite.scale, transformSprite.rotation,
-                           transformSprite.translation);
-      Matrix4x4 WVPMatrixSprite =
-          Multiply(worldMatrixSprite,
-                   Multiply(viewMatrixSprite, projectionMatrixSprite));
-      transformationMatrixDataSprite->WVP = WVPMatrixSprite;
-
-      Matrix4x4 uvTransformMatrix = MakeScaleMatrix(uvTransformSprite.scale);
-      uvTransformMatrix = Multiply(
-          uvTransformMatrix, MakeRotateMatrix(Z, uvTransformSprite.rotation.z));
-      uvTransformMatrix =
-          Multiply(uvTransformMatrix,
-                   MakeTranslateMatrix(uvTransformSprite.translation));
-      spriteMaterialData->uvTransform = uvTransformMatrix;
     }
   }
 
@@ -1197,8 +838,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   imgui.Shutdown();
 
   vertexResource->Release();
-  vertexResourceSprite->Release();
-  transformationMatrixResourceSprite->Release();
   sphereWvpResource->Release();
 
   pipeline.Term();
@@ -1212,9 +851,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   delete sphere;
 
   texMgr.Term();
-
-  indexResourceSprite->Release();
-  spriteMaterialResource->Release();
 
   triangleMaterialResource->Release();
   triangleDirectionalLightResource->Release();
